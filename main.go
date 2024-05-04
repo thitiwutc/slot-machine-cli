c22b61672cd43b4c5a6ce211b01e637b8e26bfcb
Thitiwut Chutipongwanit <thitiwut.chu@gmail.com>
2024-05-04 22:32:20 +0700

    Take bet amount arg

--- main.go
+++ main.go
@@ -1,11 +1,32 @@
 package main
 
 import (
+	"flag"
+	"fmt"
+	"os"
+	"strconv"
+
 	"github.com/thitiwut-c/slot-machine-cli/app"
 )
 
 func main() {
+	flag.Usage = func() {
+		fmt.Println("Usage: slot <bet_amount>")
+	}
+
+	if len(os.Args) != 2 {
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	betAmount, err := strconv.ParseFloat(os.Args[1], 64)
+	if err != nil {
+		fmt.Println(err)
+		flag.Usage()
+		os.Exit(1)
+	}
+
 	app := app.NewDefault()
 
-	app.Run(200)
+	app.Run(betAmount)
 }
